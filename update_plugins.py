96e602d91232521535324ceaae99cbadde5ac026
root <root@localhost.localdomain>
2017-03-10 15:11:13 +0800

    delete sources_non_forked, add yankring taglist to update_plugins

--- update_plugins.py
+++ update_plugins.py
@@ -52,6 +52,8 @@ vim-flake8 https://github.com/nvie/vim-flake8
 vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
+YankRing.vim https://github.com/vim-scripts/YankRing.vim
+taglist.vim https://github.com/vim-scripts/taglist.vim
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
